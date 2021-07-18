639f7c411ec39be22dd99d39aef9a797e1fb8026
c2h8e9d4@gmail.com <c2h8e9d4@gmail.com>
2021-07-18 19:53:57 +0300

    info page

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,12 @@
 $(function () {
-  $('.slider').slick();
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 5500,
+    arrows: false,
+  });
 });
